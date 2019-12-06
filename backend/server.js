36ef4119a085e674da6e785b53eb6d19d31c1025
Hung Nguyen <hn9581@gmail.com>
2019-12-06 00:41:37 -0500

    Added gitignore

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,10 @@ app.use(cors());
 app.use(express.json());
 
 const uri = require('../config/keys').mongoURI;
-console.log(uri);
 mongoose
-    .connect(uri, { useNewUrlParser: true})
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
-// const connection = mongoose.connection;
-// connection.once('open', () => {
-//     console.log("MongoDB database connection established successfully");
-// })
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
